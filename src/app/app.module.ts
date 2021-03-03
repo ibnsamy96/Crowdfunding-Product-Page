fd9fa315f0230c7a1ed6d989c984e1b2dd76f0f4
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-03-03 02:25:18 +0200

    add: adding product page component and the navbar component to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CoverComponent } from './product-page/cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductPageComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule
